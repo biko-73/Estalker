2b4685280191d1f07de88f4a64bf37ca7d51f347
biko-73 <57875905+biko-73@users.noreply.github.com>
2025-08-31 12:45:27 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='Estalker/main'
 
-MY_IPK="enigma2-plugin-extensions-estalker_1.07_all.ipk"
-MY_DEB="enigma2-plugin-extensions-estalker_1.07_all.deb"
+MY_IPK="enigma2-plugin-extensions-estalker_1.09_all.ipk"
+MY_DEB="enigma2-plugin-extensions-estalker_1.09_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
