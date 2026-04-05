58375f895e88631c0e9c793dab731bfe22f5e6f1
biko-73 <57875905+biko-73@users.noreply.github.com>
2026-04-05 09:45:53 +0200

    Update package version to 1.36

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='Estalker/main'
 
-MY_IPK="enigma2-plugin-extensions-estalker_1.35_all.ipk"
-MY_DEB="enigma2-plugin-extensions-estalker_1.35_all.deb"
+MY_IPK="enigma2-plugin-extensions-estalker_1.36_all.ipk"
+MY_DEB="enigma2-plugin-extensions-estalker_1.36_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
