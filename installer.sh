744859bc94b468e99bf7382413800a32f9112f91
biko-73 <57875905+biko-73@users.noreply.github.com>
2026-05-03 10:45:38 +0300

    Update package version to 1.38 in installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='Estalker/main'
 
-MY_IPK="enigma2-plugin-extensions-estalker_1.37_all.ipk"
-MY_DEB="enigma2-plugin-extensions-estalker_1.37_all.deb"
+MY_IPK="enigma2-plugin-extensions-estalker_1.38_all.ipk"
+MY_DEB="enigma2-plugin-extensions-estalker_1.38_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
